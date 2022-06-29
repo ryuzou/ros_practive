d40236e3c192f2be70866c373eba299c56d54154
Ryuzo Takahashi <ryuzot.1112@gmail.com>
2022-06-29 15:09:08 +0900

    fix

--- day2_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryuzot/ros_practice/day2_ws/src
-# Build directory: /home/ryuzot/ros_practice/day2_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("auto_py")
--- day2_ws/build/timer_rotate_py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryuzot/ros_practice/day2_ws/src/timer_rotate_py
-# Build directory: /home/ryuzot/ros_practice/day2_ws/build/timer_rotate_py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
