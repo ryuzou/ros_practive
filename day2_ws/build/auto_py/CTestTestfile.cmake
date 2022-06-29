45109b666ecde414a9957b0d29415da59c761038
Ryuzo Takahashi <ryuzot.1112@gmail.com>
2022-06-29 15:08:34 +0900

    fit things

--- day2_ws/build/auto_py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryuzot/ros_practice/day2_ws/src/auto_py
-# Build directory: /home/ryuzot/ros_practice/day2_ws/build/auto_py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
